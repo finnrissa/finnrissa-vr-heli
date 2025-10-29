4dc54e38ffeb4b98d1a885799b9ea62b798b3019
finnrissa <rissanenfinn@gmail.com>
2025-10-29 14:15:22 +0200

    updated rotor script
    yea

--- Assets/heli/RotorBladeController.cs
+++ Assets/heli/RotorBladeController.cs
@@ -31,18 +31,12 @@ public class RotorBladeController : MonoBehaviour
         else
             rotateDegrees += bladeSpeed * Time.deltaTime;
         rotateDegrees = rotateDegrees % 360;
-		switch (rotationAxis) // switch function selects between each blade axis. different cases = different rotation axes.
+		switch (rotationAxis)
 		{
-
-			case Axis.y:
-				transform.localRotation = Quaternion.Euler(rotation.x, rotateDegrees, rotation.z);
-				break;
 			case Axis.z:
 				transform.localRotation = Quaternion.Euler(rotation.x, rotation.y, rotateDegrees);
 				break;
-			case Axis.x:
-				break;
-			default: // just in case none of these cases match in the current moment
+			default:
 				transform.localRotation = Quaternion.Euler(rotateDegrees, rotation.y, rotation.z); // essentially keeps the axis cases in check in the event they do not run
                 break;
 		}
