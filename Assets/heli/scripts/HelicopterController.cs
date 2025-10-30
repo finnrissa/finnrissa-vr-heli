526a727a29a155c3c95a1ae5e98ce978448b7a39
finnrissa <rissanenfinn@gmail.com>
2025-10-30 15:56:02 +0200

    project update
    NEW Script

--- Assets/heli/scripts/HelicopterController.cs
+++ Assets/heli/scripts/HelicopterController.cs
@@ -1,19 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//hi
 public class HelicopterController : MonoBehaviour
 {
     private Rigidbody _rigidbody;
-
-    [SerializeField] private float responsiveness = 500f;
-	[SerializeField] private float yawResponsiveness = 250f;
-	[SerializeField] private float rollResponsiveness = 150f;
-	[SerializeField] private float pitchResponsiveness = 250f;
-	[SerializeField] private float throttleResponsiveness = 50f;
-	[SerializeField] private float throttleAmount = 25f;
+	[SerializeField] private float throttleSensitivity;
+	[SerializeField] private float yawRate;
+	[SerializeField] private float rollRate;
+	[SerializeField] private float pitchRate;
+	
     private float throttle;
-
     private float roll;
     private float pitch;
     private float yaw;
@@ -27,28 +24,34 @@ public class HelicopterController : MonoBehaviour
 		InputCtrl();
 	}
 
-	private void FixedUpdate()
+	private void FixedUpdate() // forces acting on the helicopter. Given it is rigidbody I'm using ForceMode. Impulse since it is weight dependent.
 	{
-        _rigidbody.AddForce(transform.up * throttle, ForceMode.Impulse);
-		_rigidbody.AddTorque(transform.right * pitch * pitchResponsiveness);
-		_rigidbody.AddTorque(transform.forward * roll * rollResponsiveness);
-		_rigidbody.AddTorque(transform.up * yaw * yawResponsiveness);
+		_rigidbody.AddForce(transform.up * throttle, ForceMode.Impulse);
+		_rigidbody.AddTorque(transform.right * pitch * pitchRate, ForceMode.Acceleration);
+		_rigidbody.AddTorque(transform.forward * roll * rollRate, ForceMode.Acceleration);
+		_rigidbody.AddTorque(transform.up * yaw * yawRate, ForceMode.Acceleration);
 	}
-	private void InputCtrl()
+	// conversely, acceleration isn't. I don't fully understand these, though I think they are working fine, I just have no limits placed on the airframe yet.
+	private void InputCtrl() // relative control axes from input manager
     {
 		roll = Input.GetAxis("Roll");
 		pitch = Input.GetAxis("Pitch");
 		yaw = Input.GetAxis("Yaw");
 
-        if (Input.GetKey(KeyCode.LeftShift))
+		// Throttle axis percentage and 0% clamp
+		if (Input.GetKey(KeyCode.LeftShift))
         {
-            throttle += Time.deltaTime * throttleAmount;
+            throttle += throttleSensitivity;
         }
         else if (Input.GetKey(KeyCode.LeftControl))
         {
-            throttle -= Time.deltaTime * throttleAmount;
+			throttle -= throttleSensitivity;
         }
+		Debug.Log(roll);
+		Debug.Log(pitch);
+		Debug.Log(yaw);
+		Debug.Log(throttle);
 
-        throttle = Mathf.Clamp(throttle, 0f, 100f);
-    }
+		throttle = Mathf.Clamp(throttle, 0f, 100f);
+	}
 }
