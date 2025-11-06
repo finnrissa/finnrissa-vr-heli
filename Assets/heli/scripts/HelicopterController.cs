64f73f54a268810cfc1a1603a44d87c75660b765
finnrissa <rissanenfinn@gmail.com>
2025-11-06 13:56:46 +0200

    IT WORKS.PNG
    it lowkey works
    still need to bind yaw, but before that happens i need to add airframe limitations so like
    you can fly it
    throttle also needs work

--- Assets/heli/scripts/HelicopterController.cs
+++ Assets/heli/scripts/HelicopterController.cs
@@ -18,19 +18,38 @@ public class HelicopterController : MonoBehaviour
     private float pitch;
     private float yaw;
 
+	public float throttle2;
+	public float roll2;
+	public float pitch2;
+	public float yaw2;
+
 	private void Awake()
 	{
 		_rigidbody = GetComponent<Rigidbody>();
 	}
 	private void Update()
 	{
-		
-		if (InputBridge.Instance.RightTrigger > 0)
-		{
-			Debug.Log("Right Trigger is being held down!");
-		}
 		InputCtrl();
-		
+
+		if (InputBridge.Instance.RightThumbstickAxis.x > 0)
+		{
+			Debug.Log("Stick POSITIVE X.");
+			if (InputBridge.Instance.RightThumbstickAxis.x == 0)
+			{
+				Debug.Log("Stick X UNACTUATED");
+			}
+		}
+		else Debug.Log("Stick NEGATIVE X");
+
+		if (InputBridge.Instance.RightThumbstickAxis.y > 0)
+		{
+			Debug.Log("Stick POSITIVE Y");
+			if (InputBridge.Instance.RightThumbstickAxis.y == 0)
+			{
+				Debug.Log("Stick Y UNACTUATED");
+			}
+		}
+		else Debug.Log("Stick NEGATIVE Y.");
 	}
 
 	private void FixedUpdate() // forces acting on the helicopter. Force mode impulse since it is weight dependent. The rigidbody weighs 360kg.
@@ -39,13 +58,21 @@ public class HelicopterController : MonoBehaviour
 		_rigidbody.AddTorque(transform.right * pitch * pitchRate, ForceMode.Acceleration);
 		_rigidbody.AddTorque(transform.forward * roll * rollRate, ForceMode.Acceleration);
 		_rigidbody.AddTorque(transform.up * yaw * yawRate, ForceMode.Acceleration);
+		_rigidbody.AddForce(transform.up * throttle2, ForceMode.Impulse);
+		_rigidbody.AddTorque(transform.right * pitch2 * pitchRate, ForceMode.Acceleration);
+		_rigidbody.AddTorque(transform.forward * roll2 * rollRate, ForceMode.Acceleration);
+		_rigidbody.AddTorque(transform.up * yaw2 * yawRate, ForceMode.Acceleration);
 	}
-	// i don't fully understand these yet, though I think they are working fine as coded, I just have no limits placed on the airframe yet
+	//no limits placed on the airframe yet
 	private void InputCtrl() // relative control axes from input manager
     {
 		roll = Input.GetAxis("Roll");
 		pitch = Input.GetAxis("Pitch");
 		yaw = Input.GetAxis("Yaw");
+
+		roll2 = -InputBridge.Instance.RightThumbstickAxis.x;
+		pitch2 = InputBridge.Instance.RightThumbstickAxis.y;
+		throttle2 = InputBridge.Instance.RightTrigger;
 
 		// Throttle axis percentage and 0% clamp
 		if (Input.GetKey(KeyCode.LeftShift))
@@ -62,5 +89,16 @@ public class HelicopterController : MonoBehaviour
 		//Debug.Log(throttle);
 
 		throttle = Mathf.Clamp(throttle, 0f, 100f);
+
+		if (InputBridge.Instance.RightTriggerDown)
+		{
+			throttle2 += throttleSensitivity*10;
+		}
+		else if (InputBridge.Instance.RightGripDown)
+		{
+			throttle2 -= throttleSensitivity*10;
+		}
+
+		throttle2 = Mathf.Clamp(throttle2, 0f, 100f);
 	}
 }
